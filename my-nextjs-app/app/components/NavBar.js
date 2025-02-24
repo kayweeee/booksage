0a0aecb75586e124e96ce1a0f00fcaecd30c2e2b
kayweeee <tankaywee009@gmail.com>
2025-02-24 23:09:11 +0800

    navbar

---
+++ my-nextjs-app/app/components/NavBar.js
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <div className="pt-3 pb-6 px-6 w-full flex flex-row justify-between items-center">
+      <Link href="/" className="flex items-center">
+        <img
+          src="/Small-Logo.png"
+          alt="Book Sage Logo"
+          className="w-28 object-cover rounded-lg hover:cursor-pointer"
+        />
+      </Link>
+
+      {/* Navigation Links */}
+      <div className="flex flex-row gap-6 text-lg">
+        <Link
+          href="/"
+          className={`hover:underline ${
+            pathname === "/" ? "underline font-semibold" : ""
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={`hover:underline ${
+            pathname === "/about" ? "underline font-semibold" : ""
+          }`}
+        >
+          About
+        </Link>
+      </div>
+    </div>
+  );
+}
